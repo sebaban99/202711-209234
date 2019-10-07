91833263b70c6673b7f102fd327087dd971a53f1
Romii <romiina.r.99@gmail.com>
2019-10-07 15:33:04 -0300

    CreateAccount()  invalid format, has letters green stage and refactor

--- Obligatorio 1 - DA1/BusinessLogic/Account.cs
+++ Obligatorio 1 - DA1/BusinessLogic/Account.cs
@@ -17,7 +17,9 @@ namespace BusinessLogic
         {
             string phoneNumberFormat = aPhone.Replace(" ", "");
 
-            if (StartOfPhoneNumberAndLengthValidation(phoneNumberFormat))
+            if (PhoneNumberValidationOnlyNumbers(phoneNumberFormat) && 
+                (PhoneNumberValidationStartWithNine(phoneNumberFormat)
+                || PhoneNumberValidationStartWithZero(phoneNumberFormat)))
             {
                 this.Phone = aPhone;
                 this.Balance = DEFAULT_BALANCE;
@@ -28,10 +30,25 @@ namespace BusinessLogic
             }
         }
 
-        public bool StartOfPhoneNumberAndLengthValidation(string aPhone)
+        public bool PhoneNumberValidationOnlyNumbers(string aPhone)
         {
-            return aPhone[0] == '0' && aPhone[1] == '9' && aPhone.Length == 9 
-                || aPhone[0] == '9' && aPhone.Length == 8;
+           foreach (char c in aPhone)
+            {
+                if (c < '0' || c > '9')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool PhoneNumberValidationStartWithNine(string aPhone)
+        {
+            return aPhone[0] == '9' && aPhone.Length == 8;
+        }
+
+        public bool PhoneNumberValidationStartWithZero(string aPhone)
+        {
+            return aPhone[0] == '0' && aPhone[1] == '9' && aPhone.Length == 9;
         }
     }
 }
