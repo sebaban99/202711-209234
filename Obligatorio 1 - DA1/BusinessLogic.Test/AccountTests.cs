6d1622b4a24dfaed8b9f0b31563368b6b403d15e
Romii <romiina.r.99@gmail.com>
2019-10-07 13:09:02 -0300

    CreateAccount() other valid specifications green stage

--- Obligatorio 1 - DA1/BusinessLogic.Test/AccountTests.cs
+++ Obligatorio 1 - DA1/BusinessLogic.Test/AccountTests.cs
@@ -14,5 +14,32 @@ namespace BusinessLogic.Test
             Assert.AreEqual(ac.Balance, 0);
             Assert.AreEqual(ac.Phone, "098 204 265");
         }
+
+        [TestMethod]
+        public void CreateAccountWithSpaceAndWithoutZero()
+        {
+            Account ac = new Account("98 204 265");
+
+            Assert.AreEqual(ac.Balance, 0);
+            Assert.AreEqual(ac.Phone, "98 204 265");
+        }
+
+        [TestMethod]
+        public void CreateAccountWithZeroAndWithoutSpace()
+        {
+            Account ac = new Account("098204265");
+
+            Assert.AreEqual(ac.Balance, 0);
+            Assert.AreEqual(ac.Phone, "098204265");
+        }
+
+        [TestMethod]
+        public void CreateAccountWithoutSpaceAndZero()
+        {
+            Account ac = new Account("98204265");
+
+            Assert.AreEqual(ac.Balance, 0);
+            Assert.AreEqual(ac.Phone, "98204265");
+        }
     }
 }
