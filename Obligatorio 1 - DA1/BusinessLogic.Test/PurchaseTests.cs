91a8e4770dbd1e0da581bcfee0fd8c11fe09ecc3
Sebastián Bañales <tatantus1@gmail.com>
2019-10-08 20:50:12 -0300

    Purchase class created + Purchase constructor test red stage

--- Obligatorio 1 - DA1/BusinessLogic.Test/PurchaseTests.cs
+++ Obligatorio 1 - DA1/BusinessLogic.Test/PurchaseTests.cs
@@ -7,8 +7,23 @@ namespace BusinessLogic.Test
     public class PurchaseTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CreatePurchaseWithValid_LicensePlate_MinutesToPurchase_StartingHour()
         {
+            Account testAccount = new Account("099 123 456")
+            {
+                Balance = 500
+            };
+            int costPerMinute = 1;
+            Purchase aPurchase = new Purchase(costPerMinute, "SBS 1234 120 11:00")
+            {
+                Account = testAccount
+            };
+
+            Assert.AreEqual(aPurchase.Account, testAccount);
+            Assert.AreEqual(aPurchase.LicensePlate, "SBS 1234");
+            Assert.AreEqual(aPurchase.StartingHour.Hour, 11);
+            Assert.AreEqual(aPurchase.FinishingHour, 13);
+
         }
     }
 }
---
+++ Obligatorio 1 - DA1/BusinessLogic/Purchase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+    public class Purchase
+    {
+        
+        public Account Account { get; set; }
+        public string LicensePlate { get; set; }
+        public DateTime StartingHour { get; set; }
+        public DateTime FinishingHour { get; set; }
+
+        public Purchase(int costPerMinute, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
