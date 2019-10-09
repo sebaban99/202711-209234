0da4628ff3af6cea54b762ae86da0bb86008094c
Sebastián Bañales <tatantus1@gmail.com>
2019-10-09 13:32:53 -0300

    Purchase constructor refactor

--- Obligatorio 1 - DA1/BusinessLogic/Purchase.cs
+++ Obligatorio 1 - DA1/BusinessLogic/Purchase.cs
@@ -13,10 +13,38 @@ namespace BusinessLogic
         public Purchase(int costPerMinute, string message)
         {
             string[] messageSplit = message.Split(new Char[] { ' ' });
-            LicensePlate = messageSplit[0] + " " + messageSplit[1];
-            StartingHour = DateTime.Parse(DateTime.Today.ToString("dd/MM/yyyy") + " " + messageSplit[3]);
-            int amountOfMinutes = Int32.Parse(messageSplit[2]);
-            FinishingHour = StartingHour.AddMinutes(amountOfMinutes);
+            LicensePlate = extractLicensePlate(messageSplit);
+            StartingHour = stringToDateTime(messageSplit);
+            int amountOfMinutes = extractMinutes(messageSplit);
+            FinishingHour = calculateFinishingHour(amountOfMinutes);
+        }
+
+        private string extractLicensePlate(string[] messageSplit)
+        {
+            return messageSplit[0] + " " + messageSplit[1];
+        }
+
+        private DateTime stringToDateTime(string[] messageSplit)
+        {
+            return DateTime.Parse(getTodaysDate_dd_MM_yyyy() + " " + messageSplit[3]);
+        }
+
+        private int extractMinutes(string[] messageSplit)
+        {
+            return Int32.Parse(messageSplit[2]);
+        }
+        private string getTodaysDate_dd_MM_yyyy()
+        {
+            return DateTime.Today.ToString("dd/MM/yyyy");
+        }
+
+        private DateTime calculateFinishingHour(int amountOfMinutes)
+        {
+            if (StartingHour != null)
+                return StartingHour.AddMinutes(amountOfMinutes);
+            else
+                throw new ArgumentNullException();
+
         }
     }
 }
