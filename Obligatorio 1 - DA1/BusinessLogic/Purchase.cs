709be979dd9c230e93a17c44b6013cf9303397bd
Sebastián Bañales <tatantus1@gmail.com>
2019-10-09 15:17:12 -0300

    Purchase constructor refactor, licensePlate without space

--- Obligatorio 1 - DA1/BusinessLogic/Purchase.cs
+++ Obligatorio 1 - DA1/BusinessLogic/Purchase.cs
@@ -13,56 +13,85 @@ namespace BusinessLogic
         public Purchase(int costPerMinute, string message, Account accountReceived)
         {
             string[] messageSplit = message.Split(new Char[] { ' ' });
-            if(messageSplit.Length == 4)
+            Account = accountReceived;
+            LicensePlate = extractLicensePlate(messageSplit);
+            StartingHour = stringToDateTime(messageSplit);
+            FinishingHour = calculateFinishingHour(messageSplit);
+            DecreaseBalanceOnAccount(costPerMinute, messageSplit);
+     
+            //else
+            //{
+
+            //    LicensePlate = extractLicensePlate(messageSplit);
+            //    StartingHour = stringToDateTime(messageSplit);
+            //    int amountOfMinutes = Int32.Parse(messageSplit[1]);
+            //    FinishingHour = calculateFinishingHour(messageSplit);
+            //    int balanceToDecrease = amountOfMinutes * costPerMinute;
+            //    Account = accountReceived;
+            //    Account.DecreaseBalance(balanceToDecrease);
+            //}
+
+        }
+
+        private void DecreaseBalanceOnAccount(int costPerMinute, string[] messageSplit)
+        {
+            int balanceToDecrease = extractMinutes(messageSplit) * costPerMinute;
+            Account.DecreaseBalance(balanceToDecrease);
+        }
+
+        private string extractLicensePlate(string[] messageSplit)
+        {
+            if (messageSplit.Length == 4)
             {
-                LicensePlate = extractLicensePlate(messageSplit);
-                StartingHour = stringToDateTime(messageSplit);
-                int amountOfMinutes = extractMinutes(messageSplit);
-                FinishingHour = calculateFinishingHour(amountOfMinutes);
-                int balanceToDecrease = amountOfMinutes * costPerMinute;
-                Account = accountReceived;
-                Account.DecreaseBalance(balanceToDecrease);
+                return messageSplit[0] + " " + messageSplit[1];
             }
             else
             {
                 StringBuilder sb = new StringBuilder(messageSplit[0]);
                 sb.Insert(3, " ");
-                LicensePlate = sb.ToString();
-                StartingHour = DateTime.Parse(getTodaysDate_dd_MM_yyyy() + " " + messageSplit[2]);
-                int amountOfMinutes = Int32.Parse(messageSplit[1]);
-                FinishingHour = calculateFinishingHour(amountOfMinutes);
-                int balanceToDecrease = amountOfMinutes * costPerMinute;
-                Account = accountReceived;
-                Account.DecreaseBalance(balanceToDecrease);
+                return sb.ToString();
             }
-            
-        }
-
-        private string extractLicensePlate(string[] messageSplit)
-        {
-            return messageSplit[0] + " " + messageSplit[1];
         }
 
         private DateTime stringToDateTime(string[] messageSplit)
         {
-            return DateTime.Parse(getTodaysDate_dd_MM_yyyy() + " " + messageSplit[3]);
+            if (messageSplit.Length == 4)
+            {
+                return DateTime.Parse(getTodaysDate_dd_MM_yyyy() + " " + messageSplit[3]);
+            }
+            else
+            {
+                return DateTime.Parse(getTodaysDate_dd_MM_yyyy() + " " + messageSplit[2]);
+            }
         }
 
         private int extractMinutes(string[] messageSplit)
         {
-            return Int32.Parse(messageSplit[2]);
+            if (messageSplit.Length == 4)
+            {
+                return Int32.Parse(messageSplit[2]);
+            }
+            else
+            {
+                return Int32.Parse(messageSplit[1]);
+            }
         }
         private string getTodaysDate_dd_MM_yyyy()
         {
             return DateTime.Today.ToString("dd/MM/yyyy");
         }
 
-        private DateTime calculateFinishingHour(int amountOfMinutes)
+        private DateTime calculateFinishingHour(string[] messageSplit)
         {
             if (StartingHour != null)
-                return StartingHour.AddMinutes(amountOfMinutes);
+            {
+                return StartingHour.AddMinutes(extractMinutes(messageSplit));
+            }
             else
+            {
                 throw new ArgumentNullException();
+            }
+
 
         }
     }
