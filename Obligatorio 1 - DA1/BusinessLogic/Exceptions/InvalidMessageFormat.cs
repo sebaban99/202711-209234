cadf97326dbf179b036cc8a082fb9afa0aa480a8
Sebastián Bañales <tatantus1@gmail.com>
2019-10-09 17:06:07 -0300

    Create InvalidMessageFormat class for Exceptions

---
+++ Obligatorio 1 - DA1/BusinessLogic/Exceptions/InvalidMessageFormat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BusinessLogic.Exceptions
+{
+    public class InvalidMessageFormat : Exception
+    {
+        public InvalidMessageFormat() { }
+
+        public InvalidMessageFormat(string message) : base(message) { }
+
+        public InvalidMessageFormat(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
