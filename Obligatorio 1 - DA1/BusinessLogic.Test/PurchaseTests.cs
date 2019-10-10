bc605eb141102dbe72162cb3b82e5a84eaa49c7c
Sebastián Bañales <tatantus1@gmail.com>
2019-10-10 15:51:56 -0300

    Update on last commit
    Forgot to add Exception class file + PurchaseTests file. No code modifications, only adding files

--- Obligatorio 1 - DA1/BusinessLogic.Test/PurchaseTests.cs
+++ Obligatorio 1 - DA1/BusinessLogic.Test/PurchaseTests.cs
@@ -13,14 +13,12 @@ namespace BusinessLogic.Test
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
             
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "SBS 1234 120 13:00", testAccount);
+            Purchase aPurchase = new Purchase("SBS 1234 120 13:00");
 
-            Assert.AreEqual(aPurchase.Account, testAccount);
             Assert.AreEqual(aPurchase.LicensePlate, "SBS 1234");
             Assert.AreEqual(aPurchase.StartingHour.Hour, 13);
             Assert.AreEqual(aPurchase.FinishingHour.Hour, 15);
-            Assert.AreEqual(aPurchase.Account.Balance, 380);
+            
         }
 
         [TestMethod]
@@ -29,14 +27,12 @@ namespace BusinessLogic.Test
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "SBS1234 120 13:00", testAccount);
+            
+            Purchase aPurchase = new Purchase("SBS1234 120 13:00");
 
-            Assert.AreEqual(aPurchase.Account, testAccount);
             Assert.AreEqual(aPurchase.LicensePlate, "SBS 1234");
             Assert.AreEqual(aPurchase.StartingHour.Hour, 13);
             Assert.AreEqual(aPurchase.FinishingHour.Hour, 15);
-            Assert.AreEqual(aPurchase.Account.Balance, 380);
 
         }
 
@@ -46,19 +42,16 @@ namespace BusinessLogic.Test
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "SBS1234 120", testAccount);
+            Purchase aPurchase = new Purchase("SBS1234 120");
             DateTime now = DateTime.Now;
             DateTime finishTime = now.AddMinutes(120);
 
-            Assert.AreEqual(aPurchase.Account, testAccount);
             Assert.AreEqual(aPurchase.LicensePlate, "SBS 1234");
             Assert.AreEqual(aPurchase.StartingHour.Hour, now.Hour);
             Assert.AreEqual(aPurchase.StartingHour.Minute, now.Minute);
             Assert.AreEqual(aPurchase.FinishingHour.Hour, finishTime.Hour);
             Assert.AreEqual(aPurchase.FinishingHour.Minute, finishTime.Minute);
-            Assert.AreEqual(aPurchase.Account.Balance, 380);
+
         }
 
         [TestMethod]
@@ -68,18 +61,16 @@ namespace BusinessLogic.Test
             testAccount.IncreaseBalance(500);
 
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "SBS 1234 120", testAccount);
+            
+            Purchase aPurchase = new Purchase("SBS 1234 120");
             DateTime now = DateTime.Now;
             DateTime finishTime = now.AddMinutes(120);
 
-            Assert.AreEqual(aPurchase.Account, testAccount);
             Assert.AreEqual(aPurchase.LicensePlate, "SBS 1234");
             Assert.AreEqual(aPurchase.StartingHour.Hour, now.Hour);
             Assert.AreEqual(aPurchase.StartingHour.Minute, now.Minute);
             Assert.AreEqual(aPurchase.FinishingHour.Hour, finishTime.Hour);
             Assert.AreEqual(aPurchase.FinishingHour.Minute, finishTime.Minute);
-            Assert.AreEqual(aPurchase.Account.Balance, 380);
         }
 
         [TestMethod]
@@ -88,147 +79,135 @@ namespace BusinessLogic.Test
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "SBS 1234 120  13:00", testAccount);
+           
+            Purchase aPurchase = new Purchase("SBS 1234 120  13:00");
 
-            Assert.AreEqual(aPurchase.Account, testAccount);
+            
             Assert.AreEqual(aPurchase.LicensePlate, "SBS 1234");
             Assert.AreEqual(aPurchase.StartingHour.Hour, 13);
             Assert.AreEqual(aPurchase.FinishingHour.Hour, 15);
-            Assert.AreEqual(aPurchase.Account.Balance, 380);
+            
 
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidMessageFormatException))]
+        [ExpectedException(typeof(BusinessException))]
         public void CreatePurchaseInvalidParameters_WrongLicensePlate_MissingLetters()
         {
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "1234 120 13:00", testAccount);
+            Purchase aPurchase = new Purchase("1234 120 13:00");
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidMessageFormatException))]
+        [ExpectedException(typeof(BusinessException))]
         public void CreatePurchaseInvalidParameters_WrongLicensePlate_InvalidFormatXXX()
         {
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "SB1 1234 120 13:00", testAccount);
+            Purchase aPurchase = new Purchase("SB1 1234 120 13:00");
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidMessageFormatException))]
+        [ExpectedException(typeof(BusinessException))]
         public void CreatePurchaseInvalidParameters_WrongLicensePlate_InvalidFormatYYYY()
         {
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "SBA 1T34 120 13:00", testAccount);
+            Purchase aPurchase = new Purchase("SBA 1T34 120 13:00");
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidMessageFormatException))]
+        [ExpectedException(typeof(BusinessException))]
         public void CreatePurchaseInvalidParameters_WrongLicensePlate_MissingNumbers()
         {
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "ABs 120 13:00", testAccount);
+            Purchase aPurchase = new Purchase("ABs 120 13:00");
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidMessageFormatException))]
+        [ExpectedException(typeof(BusinessException))]
         public void CreatePurchaseInvalidParameters_WrongLicensePlate_XXXContainsLetters()
         {
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "AB43456 120 13:00", testAccount);
+            Purchase aPurchase = new Purchase("AB43456 120 13:00");
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidMessageFormatException))]
+        [ExpectedException(typeof(BusinessException))]
         public void CreatePurchaseInvalidParameters_WrongLicensePlate_YYYYContainsNumbers()
         {
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "rBA34u6 120 13:00", testAccount);
+            Purchase aPurchase = new Purchase("rBA34u6 120 13:00");
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidMessageFormatException))]
+        [ExpectedException(typeof(BusinessException))]
         public void CreatePurchaseInvalidParameters_MinutesAreNotMultipleOf30()
         {
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "AzA 1237 110 13:00", testAccount);
+            Purchase aPurchase = new Purchase("AzA 1237 110 13:00");
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidMessageFormatException))]
+        [ExpectedException(typeof(BusinessException))]
         public void CreatePurchaseInvalidParameters_InvalidFormat_StartingHour_HHmm_Variant1()
         {
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
-
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "AzA 1237 120 9", testAccount);
+            
+            Purchase aPurchase = new Purchase("AzA 1237 120 9");
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidMessageFormatException))]
+        [ExpectedException(typeof(BusinessException))]
         public void CreatePurchaseInvalidParameters_InvalidFormat_StartingHour_HHmm_Variant2()
         {
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "AzA 1237 120 9:0", testAccount);
+            Purchase aPurchase = new Purchase("AzA 1237 120 9:0");
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidMessageFormatException))]
+        [ExpectedException(typeof(BusinessException))]
         public void CreatePurchaseInvalidParameters_InvalidFormat_StartingHour_HHmm_ContainsLetters()
         {
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "AzA 1237 120 09:AM", testAccount);
+            Purchase aPurchase = new Purchase("AzA 1237 120 09:AM");
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidMessageFormatException))]
+        [ExpectedException(typeof(BusinessException))]
         public void CreatePurchaseInvalidParameters_MinutesAreZero()
         {
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "AzA 1237 0 13:00", testAccount);
+            Purchase aPurchase = new Purchase("AzA 1237 0 13:00");
         }
-
+        
         [TestMethod]
-        [ExpectedException(typeof(InvalidMessageFormatException))]
+        [ExpectedException(typeof(BusinessException))]
         public void CreatePurchaseInvalidParameters_InvalidStartingHour_BeforeMinimum()
         {
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "AzA 1237 120 09:00", testAccount);
+            Purchase aPurchase = new Purchase("AzA 1237 120 09:00");
         }
 
         [TestMethod]
@@ -237,44 +216,37 @@ namespace BusinessLogic.Test
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "SBS 1234 120 10:00", testAccount);
-
-            Assert.AreEqual(aPurchase.Account, testAccount);
+            Purchase aPurchase = new Purchase("SBS 1234 120 10:00");
             Assert.AreEqual(aPurchase.LicensePlate, "SBS 1234");
             Assert.AreEqual(aPurchase.StartingHour.Hour, 10);
             Assert.AreEqual(aPurchase.FinishingHour.Hour, 12);
-            Assert.AreEqual(aPurchase.Account.Balance, 380);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidMessageFormatException))]
+        [ExpectedException(typeof(BusinessException))]
         public void CreatePurchaseInvalidParameters_InvalidStartingHour_AfterMaximum()
         {
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "AzA 1237 120 19:00", testAccount);
+            Purchase aPurchase = new Purchase("AzA 1237 120 19:00");
         }
 
         [TestMethod]
-        
         public void CreatePurchaseValidParameters_StartingTimePlusMinutesExceedsMaximumHour()
         {
             Account testAccount = new Account("099 123 456");
             testAccount.IncreaseBalance(500);
 
-            int costPerMinute = 1;
-            Purchase aPurchase = new Purchase(costPerMinute, "AzA 1237 120 17:35", testAccount);
+            Purchase aPurchase = new Purchase("AzA 1237 120 17:35");
 
-            Assert.AreEqual(aPurchase.Account, testAccount);
+            
             Assert.AreEqual(aPurchase.LicensePlate, "AzA 1237");
             Assert.AreEqual(aPurchase.StartingHour.Hour, 17);
             Assert.AreEqual(aPurchase.StartingHour.Minute, 35);
             Assert.AreEqual(aPurchase.FinishingHour.Hour, 18);
             Assert.AreEqual(aPurchase.FinishingHour.Minute, 0);
-            Assert.AreEqual(aPurchase.Account.Balance, 415);
+            
         }
 
 
