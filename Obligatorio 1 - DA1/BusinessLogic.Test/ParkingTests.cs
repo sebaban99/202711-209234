e0cb73482e1186cffaaa5d8399d6ff8a3056040a
Romii <romiina.r.99@gmail.com>
2019-10-10 16:53:17 -0300

    Create ParkingTests class. Parking() test and refactor

---
+++ Obligatorio 1 - DA1/BusinessLogic.Test/ParkingTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessLogic.Test
+{
+    [TestClass]
+    public class ParkingTests
+    {
+
+        [TestMethod]
+        public void CreateParking()
+        {
+            Parking aParking = new Parking();
+
+            Assert.AreEqual(aParking.GetAllAccounts().Count, 0);
+            Assert.AreEqual(aParking.GetAllPurchases().Count, 0);
+            Assert.AreEqual(aParking.CostPerMinute, 1);
+        }
+    }
+}
---
+++ Obligatorio 1 - DA1/BusinessLogic/Parking.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessLogic
+{
+    public class Parking
+    {
+        private List<Account> accounts;
+        private List<Purchase> purchases;
+        public int CostPerMinute { get; set; }
+
+        public Parking()
+        {
+            this.accounts = new List<Account>();
+            this.purchases = new List<Purchase>();
+            this.CostPerMinute = 1;
+        }
+
+        public List<Account> GetAllAccounts()
+        {
+            return accounts;
+        }
+
+        public List<Purchase> GetAllPurchases()
+        {
+            return purchases;
+        }
+    }
+}
