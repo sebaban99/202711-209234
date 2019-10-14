1edde8e7027c68836d6d8b111fb5e002bc26f903
Sebastián Bañales <tatantus1@gmail.com>
2019-10-14 16:58:32 -0300

    Refactor Parking class
    Now Parking validates, phoneNumbers and licensePlates, as we thought it was better done this way than the original planning stated. Plus added isPurchaseActive method to check if a Purchase is active. Added test methods to test all of this, each one with red/green/refactor.

--- Obligatorio 1 - DA1/BusinessLogic/Parking.cs
+++ Obligatorio 1 - DA1/BusinessLogic/Parking.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using BusinessLogic.Exceptions;
 
 namespace BusinessLogic
 {
@@ -36,11 +39,11 @@ namespace BusinessLogic
             purchases.Add(aPurchase);
         }
 
-        public bool ExistAccount(Account newAccount)
+        public bool ExistAccount(Account accountReceived)
         {
             foreach (Account ac in accounts)
             {
-                if (ac.Phone == newAccount.Phone)
+                if (ac.Phone == accountReceived.Phone)
                 {
                     return true;
                 }
@@ -54,7 +57,142 @@ namespace BusinessLogic
             {
                 return true;
             }
-            throw new ArgumentException("Saldo insuficiente");
+            throw new BusinessException("Saldo insuficiente");
+        }
+
+
+        public bool IsNumberPhoneValid(string aPhone)
+        {
+            string actualPhoneNumber = RemoveSpacesString(aPhone);
+
+            if (ContainsNumbersOnly(actualPhoneNumber) &&
+                (PhoneNumberValidationStartingWithNine(actualPhoneNumber)
+                || PhoneNumberValidationStartingWithZero(actualPhoneNumber)))
+                return true;
+            else
+            {
+                throw new BusinessException("Formato de número de telefono incorrecto");
+            }
+        }
+
+        private string RemoveSpacesString(string text)
+        {
+            StringBuilder stringWithoutSpaces = new StringBuilder(text);
+            stringWithoutSpaces.Replace(" ", "");
+            return stringWithoutSpaces.ToString();
+        }
+
+        private bool ContainsNumbersOnly(string message)
+        {
+            return Regex.IsMatch(message, @"^[0-9]+$");
+        }
+
+        private bool ContainsLettersOnly(string message)
+        {
+            return Regex.IsMatch(message, @"^[a-zA-Z]+$");
+        }
+
+        private bool PhoneNumberValidationStartingWithNine(string aPhone)
+        {
+            return aPhone[0] == '9' && aPhone.Length == 8;
+        }
+
+        private bool PhoneNumberValidationStartingWithZero(string aPhone)
+        {
+            return aPhone[0] == '0' && aPhone[1] == '9' && aPhone.Length == 9;
+        }
+
+        public string FormatPhoneNumber(string aPhone)
+        {
+            aPhone = RemoveSpacesString(aPhone);
+            StringBuilder formattedNumber = new StringBuilder(aPhone);
+
+            if (formattedNumber.ToString().Length == 8)
+            {
+                formattedNumber.Insert(0, 0);
+            }
+            formattedNumber.Insert(3, " ");
+            formattedNumber.Insert(7, " ");
+            return formattedNumber.ToString();
+        }
+
+        public bool IsLicensePlateValid(string licensePlate)
+        {
+            licensePlate = RemoveSpacesString(licensePlate);
+            if (licensePlate.Length == 7 && ContainsLettersOnly(licensePlate.Substring(0, 3)) &&
+                    ContainsNumbersOnly(licensePlate.Substring(3)))
+            {
+                return true;
+            }
+            else
+            {
+                throw new BusinessException("Formato de matricula incorrecto");
+            }
+        }
+
+        public bool IsPurchaseActive(string licencePlateToConfirm, DateTime theMoment)
+        {
+            licencePlateToConfirm = FormatLicencePlate(licencePlateToConfirm);
+            if (IsDateChosenInRange(theMoment))
+            {
+                foreach (Purchase p in purchases)
+                {
+                    if (p.LicensePlate.Equals(licencePlateToConfirm) &&
+                        IsPurchaseInRange(p, theMoment))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool IsDateChosenInRange(DateTime theMoment)
+        {
+            if (theMoment.Date <= DateTime.Today.Date &&
+                theMoment.Hour >= 10 && theMoment.Hour < 18)
+            {
+                return true;
+            }
+            else
+            {
+                throw new BusinessException("Error, elija una hora para el dia de hoy que este entre las 10:00 y las 18:00");
+            }
+        }
+
+        private bool IsPurchaseInRange(Purchase aPurchase, DateTime theMoment)
+        {
+            return theMoment >= aPurchase.StartingHour &&
+                theMoment <= aPurchase.FinishingHour;
+        }
+
+        private string FormatLicencePlate(string licencePlate)
+        {
+            licencePlate = RemoveSpacesString(licencePlate);
+            StringBuilder formattedLicensePlate = new StringBuilder(licencePlate);
+            formattedLicensePlate.Replace(formattedLicensePlate.ToString().Substring(0, 3),
+                formattedLicensePlate.ToString().Substring(0, 3).ToUpper().Trim());
+            formattedLicensePlate.Insert(3, " ");
+            return formattedLicensePlate.ToString();
+        }
+
+        public void MakePurchase(Account anAccount, Purchase aPurchase)
+        {
+            if (ExistAccount(anAccount))
+            {
+                if (HasEnoughBalance(anAccount, aPurchase))
+                {
+                    this.AddPurchase(aPurchase);
+                }
+                else
+                {
+                    throw new BusinessException("Saldo insuficiente");
+                }
+            }
+            else
+            {
+                throw new BusinessException("Móvil no registrado");
+            }
         }
     }
 }
