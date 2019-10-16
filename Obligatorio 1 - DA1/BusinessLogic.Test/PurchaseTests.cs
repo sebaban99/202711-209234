e8afd4eeb6febf0d89c6223a845c67b027931150
Sebastián Bañales <tatantus1@gmail.com>
2019-10-15 21:46:40 -0300

    Refactored Purchase class + Added test methods
    Added test method for invalid hour of purchase when hour is less than actual hour but between 10 and 18. Changed names of test methods adding the type of message being used in each test.
    Refactored CalculateStartingHour method and changed exception messages of this and other methods to be more specific.

--- Obligatorio 1 - DA1/BusinessLogic.Test/PurchaseTests.cs
+++ Obligatorio 1 - DA1/BusinessLogic.Test/PurchaseTests.cs
@@ -8,44 +8,27 @@ namespace BusinessLogic.Test
     public class PurchaseTests
     {
         [TestMethod]
-        public void CreatePurchaseAllValid_AllSpaces()
+        public void CreatePurchaseType1_AllValid_AllSpaces()
         {
             Purchase aPurchase = new Purchase("SBs 1234 120 13:00");
 
             Assert.AreEqual(aPurchase.LicensePlate, "SBS 1234");
             Assert.AreEqual(aPurchase.StartingHour.Hour, 13);
             Assert.AreEqual(aPurchase.FinishingHour.Hour, 15);
-
         }
 
         [TestMethod]
-        public void CreatePurchaseAllValid_NoSpaceLicensePlate()
+        public void CreatePurchaseType2_AllValid_NoSpaceLicensePlate()
         {
             Purchase aPurchase = new Purchase("SBs1234 120 13:00");
 
             Assert.AreEqual(aPurchase.LicensePlate, "SBS 1234");
             Assert.AreEqual(aPurchase.StartingHour.Hour, 13);
             Assert.AreEqual(aPurchase.FinishingHour.Hour, 15);
-
         }
 
         [TestMethod]
-        public void CreatePurchaseAllValid_NoStartingTime_NoSpaceLicensePlate()
-        {
-            Purchase aPurchase = new Purchase("SBS1234 120");
-            DateTime now = DateTime.Now;
-            DateTime finishTime = now.AddMinutes(120);
-
-            Assert.AreEqual(aPurchase.LicensePlate, "SBS 1234");
-            Assert.AreEqual(aPurchase.StartingHour.Hour, now.Hour);
-            Assert.AreEqual(aPurchase.StartingHour.Minute, now.Minute);
-            Assert.AreEqual(aPurchase.FinishingHour.Hour, finishTime.Hour);
-            Assert.AreEqual(aPurchase.FinishingHour.Minute, finishTime.Minute);
-
-        }
-
-        [TestMethod]
-        public void CreatePurchaseAllValid_NoStartingTime()
+        public void CreatePurchaseType3_AllValid_NoStartingTime()
         {
             Purchase aPurchase = new Purchase("SBS 1234 120");
             DateTime now = DateTime.Now;
@@ -59,7 +42,21 @@ namespace BusinessLogic.Test
         }
 
         [TestMethod]
-        public void CreatePurchaseValidParametersInvalidFormat_ExtraSpace()
+        public void CreatePurchaseType4_AllValid_NoStartingTime_NoSpaceLicensePlate()
+        {
+            Purchase aPurchase = new Purchase("SBS1234 120");
+            DateTime now = DateTime.Now;
+            DateTime finishTime = now.AddMinutes(120);
+
+            Assert.AreEqual(aPurchase.LicensePlate, "SBS 1234");
+            Assert.AreEqual(aPurchase.StartingHour.Hour, now.Hour);
+            Assert.AreEqual(aPurchase.StartingHour.Minute, now.Minute);
+            Assert.AreEqual(aPurchase.FinishingHour.Hour, finishTime.Hour);
+            Assert.AreEqual(aPurchase.FinishingHour.Minute, finishTime.Minute);
+        }
+
+        [TestMethod]
+        public void CreatePurchaseType1_ValidParametersInvalidFormat_ExtraSpace()
         {
             Purchase aPurchase = new Purchase("SBS 1234 120  13:00");
 
@@ -67,8 +64,6 @@ namespace BusinessLogic.Test
             Assert.AreEqual(aPurchase.LicensePlate, "SBS 1234");
             Assert.AreEqual(aPurchase.StartingHour.Hour, 13);
             Assert.AreEqual(aPurchase.FinishingHour.Hour, 15);
-
-
         }
 
         [TestMethod]
@@ -80,14 +75,14 @@ namespace BusinessLogic.Test
 
         [TestMethod]
         [ExpectedException(typeof(BusinessException))]
-        public void CreatePurchaseInvalidParameters_WrongLicensePlate_InvalidFormatXXX()
+        public void CreatePurchaseType1_InvalidParameters_WrongLicensePlate_InvalidFormatXXX()
         {
             Purchase aPurchase = new Purchase("SB1 1234 120 13:00");
         }
 
         [TestMethod]
         [ExpectedException(typeof(BusinessException))]
-        public void CreatePurchaseInvalidParameters_WrongLicensePlate_InvalidFormatYYYY()
+        public void CreatePurchaseType1_InvalidParameters_WrongLicensePlate_InvalidFormatYYYY()
         {
             Purchase aPurchase = new Purchase("SBA 1T34 120 13:00");
         }
@@ -101,62 +96,62 @@ namespace BusinessLogic.Test
 
         [TestMethod]
         [ExpectedException(typeof(BusinessException))]
-        public void CreatePurchaseInvalidParameters_WrongLicensePlate_XXXContainsLetters()
+        public void CreatePurchaseType2_InvalidParameters_WrongLicensePlate_XXXContainsLetters()
         {
             Purchase aPurchase = new Purchase("AB43456 120 13:00");
         }
 
         [TestMethod]
         [ExpectedException(typeof(BusinessException))]
-        public void CreatePurchaseInvalidParameters_WrongLicensePlate_YYYYContainsNumbers()
+        public void CreatePurchaseType2_InvalidParameters_WrongLicensePlate_YYYYContainsNumbers()
         {
             Purchase aPurchase = new Purchase("rBA34u6 120 13:00");
         }
 
         [TestMethod]
         [ExpectedException(typeof(BusinessException))]
-        public void CreatePurchaseInvalidParameters_MinutesAreNotMultipleOf30()
+        public void CreatePurchaseType1_InvalidParameters_MinutesAreNotMultipleOf30()
         {
             Purchase aPurchase = new Purchase("AzA 1237 110 13:00");
         }
 
         [TestMethod]
         [ExpectedException(typeof(BusinessException))]
-        public void CreatePurchaseInvalidParameters_InvalidFormat_StartingHour_HHmm_Variant1()
+        public void CreatePurchaseType1_InvalidParameters_InvalidFormat_StartingHour_HHmm_Variant1()
         {
             Purchase aPurchase = new Purchase("AzA 1237 120 9");
         }
 
         [TestMethod]
         [ExpectedException(typeof(BusinessException))]
-        public void CreatePurchaseInvalidParameters_InvalidFormat_StartingHour_HHmm_Variant2()
+        public void CreatePurchaseType1_InvalidParameters_InvalidFormat_StartingHour_HHmm_Variant2()
         {
             Purchase aPurchase = new Purchase("AzA 1237 120 9:0");
         }
 
         [TestMethod]
         [ExpectedException(typeof(BusinessException))]
-        public void CreatePurchaseInvalidParameters_InvalidFormat_StartingHour_HHmm_ContainsLetters()
+        public void CreatePurchaseType1_InvalidParameters_InvalidFormat_StartingHour_HHmm_ContainsLetters()
         {
             Purchase aPurchase = new Purchase("AzA 1237 120 09:AM");
         }
 
         [TestMethod]
         [ExpectedException(typeof(BusinessException))]
-        public void CreatePurchaseInvalidParameters_MinutesAreZero()
+        public void CreatePurchaseType1_InvalidParameters_MinutesAreZero()
         {
             Purchase aPurchase = new Purchase("AzA 1237 0 13:00");
         }
 
         [TestMethod]
         [ExpectedException(typeof(BusinessException))]
-        public void CreatePurchaseInvalidParameters_InvalidStartingHour_BeforeMinimum()
+        public void CreatePurchaseType1_InvalidParameters_InvalidStartingHour_BeforeMinimum()
         {
             Purchase aPurchase = new Purchase("AzA 1237 120 09:00");
         }
 
         [TestMethod]
-        public void CreatePurchaseValidParameters_StartingHourBorderCase_MinBorder()
+        public void CreatePurchaseType1_ValidParameters_StartingHourBorderCase_MinBorder()
         {
             Purchase aPurchase = new Purchase("SBS 1234 120 10:00");
 
@@ -167,20 +162,20 @@ namespace BusinessLogic.Test
 
         [TestMethod]
         [ExpectedException(typeof(BusinessException))]
-        public void CreatePurchaseInvalidParameters_StartingHourBorderCase_MaxBorder()
+        public void CreatePurchaseType1_InvalidParameters_StartingHourBorderCase_MaxBorder()
         {
             Purchase aPurchase = new Purchase("AzA 1237 120 18:00");
         }
 
         [TestMethod]
         [ExpectedException(typeof(BusinessException))]
-        public void CreatePurchaseInvalidParameters_InvalidStartingHour_AfterMaximum()
+        public void CreatePurchaseType1_InvalidParameters_InvalidStartingHour_AfterMaximum()
         {
             Purchase aPurchase = new Purchase("AzA 1237 120 19:00");
         }
 
         [TestMethod]
-        public void CreatePurchaseValidParameters_StartingTimePlusMinutesExceedsMaximumHour()
+        public void CreatePurchaseType1_ValidParameters_StartingTimePlusMinutesExceedsMaximumHour()
         {
             Purchase aPurchase = new Purchase("AzA 1237 120 17:35");
 
@@ -194,9 +189,16 @@ namespace BusinessLogic.Test
 
         [TestMethod]
         [ExpectedException(typeof(BusinessException))]
-        public void CreatePurchaseInvalidParameters_InvalidStarting()
+        public void CreatePurchaseType1_InvalidParameters_InvalidStartingHourFormat()
         {
             Purchase aPurchase = new Purchase("AzA 1237 120 10:90");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BusinessException))]
+        public void CreatePurchaseType1_InvalidStartingHour_BeforeActualHour()
+        {
+            Purchase aPurchase = new Purchase("AzA 1237 120 13:00");
         }
     }
 }
