d57e4baa83303191e3fb311b0013ec581991bb3e
Sebastián Bañales <tatantus1@gmail.com>
2019-11-07 18:08:43 -0300

    IsMessageValid refactor + added test method
    Valid message length refactor + added test method with extra space also valid.

--- Obligatorio 1 - DA1/BusinessLogic.Test/ArgentinaTests.cs
+++ Obligatorio 1 - DA1/BusinessLogic.Test/ArgentinaTests.cs
@@ -97,5 +97,17 @@ namespace BusinessLogic.Test
             mockedArg.Setup(m => m.GetDateTimeNow()).Returns(aDate);
             Assert.IsTrue(mockedArg.Object.IsMessageValid("ABC1234 10:30 50"));
         }
+
+        [TestMethod]
+        public void ValidateMessageValidMessageContainsExtraSpace()
+        {
+            Mock<Argentina> mockedArg = new Mock<Argentina>();
+            DateTime aDate = DateTime.Today;
+            aDate = aDate.AddHours(10);
+            mockedArg.Setup(m => m.GetDateTimeNow()).Returns(aDate);
+            Assert.IsTrue(mockedArg.Object.IsMessageValid("ABC1234 10:30  50"));
+        }
+
+
     }
 }
