ac7b9e0ab3d8c1800c7aec34e6c332d41d5ed5e7
Sebastián Bañales <tatantus1@gmail.com>
2019-11-07 18:49:02 -0300

    IsMessageValid red stage
    Invalid message, license plate missing YYYY (numbers)

--- Obligatorio 1 - DA1/BusinessLogic.Test/ArgentinaTests.cs
+++ Obligatorio 1 - DA1/BusinessLogic.Test/ArgentinaTests.cs
@@ -129,6 +129,16 @@ namespace BusinessLogic.Test
             Assert.IsTrue(mockedArg.Object.IsMessageValid("1234 10:30 50"));
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(BusinessException))]
+        public void ValidateMessageInvalidMessageLicensePlateMissingNumbers()
+        {
+            Mock<Argentina> mockedArg = new Mock<Argentina>();
+            DateTime aDate = DateTime.Today;
+            aDate = aDate.AddHours(10);
+            mockedArg.Setup(m => m.GetDateTimeNow()).Returns(aDate);
+            Assert.IsTrue(mockedArg.Object.IsMessageValid("ABC 10:30 50"));
+        }
 
 
     }
