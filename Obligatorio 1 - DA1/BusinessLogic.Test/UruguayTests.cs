c9043d65c5a964d107f12c517a64d16002418724
Sebastián Bañales <tatantus1@gmail.com>
2019-11-10 23:46:50 -0300

    IsPhoneNumberValid refactor
    Created Uruguay class and test class. No functionality added, just moved tests methods from ParkingTests to new class UruguayTests and method to validate phone number from Parking to Uruguay

---
+++ Obligatorio 1 - DA1/BusinessLogic.Test/UruguayTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic.Exceptions;
+using Moq;
+
+namespace BusinessLogic.Test
+{
+    [TestClass]
+    public class UruguayTests
+    {
+        private Uruguay uy;
+
+        [TestInitialize]
+        public void SetUpUruguay()
+        {
+            uy = new Uruguay();
+        }
+
+        [TestMethod]
+        public void ValidatePhoneNumberWithSpaceAndZero()
+        {
+            Assert.IsTrue(uy.IsPhoneNumberValid("098 204 265"));
+        }
+
+        [TestMethod]
+        public void ValidatePhoneNumberWithZeroAndWithoutSpace()
+        {
+            Assert.IsTrue(uy.IsPhoneNumberValid("098204265"));
+        }
+
+        [TestMethod]
+        public void ValidatePhoneNumberWithSpaceAndWithoutZero()
+        {
+            Assert.IsTrue(uy.IsPhoneNumberValid("98 204 265"));
+        }
+
+        [TestMethod]
+        public void ValidatePhoneNumberWithoutSpaceAndWithoutZero()
+        {
+            Assert.IsTrue(uy.IsPhoneNumberValid("98204265"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BusinessException))]
+        public void ValidatePhoneNumberStaringWithZeroNotFollowedByNine()
+        {
+            uy.IsPhoneNumberValid("082 042 656");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BusinessException))]
+        public void ValidatePhoneNumberNotStaringWithZeroNorNine()
+        {
+            uy.IsPhoneNumberValid("82 042 656");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BusinessException))]
+        public void ValidatePhoneNumberTooManyNumbers()
+        {
+            uy.IsPhoneNumberValid("9820426568435131");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BusinessException))]
+        public void ValidatePhoneNumberNotEnoughNumbers()
+        {
+            uy.IsPhoneNumberValid("98204");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BusinessException))]
+        public void ValidatePhoneNumberContainsLetters()
+        {
+            uy.IsPhoneNumberValid("098 740 mal");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BusinessException))]
+        public void ValidatePhoneStartWithNineInvalidLength()
+        {
+            uy.IsPhoneNumberValid("982 004 658");
+        }
+    }
+}
---
+++ Obligatorio 1 - DA1/BusinessLogic/Uruguay.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using BusinessLogic.Exceptions;
+
+namespace BusinessLogic
+{
+    public class Uruguay
+    {
+        public Uruguay() { }
+
+        public bool IsPhoneNumberValid(string phoneNumber)
+        {
+            string actualPhoneNumber = RemoveSpacesString(phoneNumber);
+            if (ContainsNumbersOnly(actualPhoneNumber) &&
+                (PhoneNumberValidationStartingWithNine(actualPhoneNumber)
+                || PhoneNumberValidationStartingWithZero(actualPhoneNumber)))
+            {
+                return true;
+            }
+            else
+            {
+                throw new BusinessException("Formato de número de telefono incorrecto");
+            }
+        }
+
+        private string RemoveSpacesString(string text)
+        {
+            StringBuilder stringWithoutSpaces = new StringBuilder(text);
+            stringWithoutSpaces.Replace(" ", "");
+            return stringWithoutSpaces.ToString();
+        }
+
+        private bool ContainsNumbersOnly(string message)
+        {
+            return Regex.IsMatch(message, @"^[0-9]+$");
+        }
+
+        private bool ContainsLettersOnly(string message)
+        {
+            return Regex.IsMatch(message, @"^[a-zA-Z]+$");
+        }
+
+        private bool PhoneNumberValidationStartingWithNine(string aPhone)
+        {
+            return aPhone[0] == '9' && aPhone.Length == 8;
+        }
+
+        private bool PhoneNumberValidationStartingWithZero(string aPhone)
+        {
+            return aPhone[0] == '0' && aPhone[1] == '9' && aPhone.Length == 9;
+        }
+    }
+}
