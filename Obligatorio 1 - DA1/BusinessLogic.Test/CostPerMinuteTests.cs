65a57db3525b6cfba2b412625be10c318c081b94
Sebastián Bañales <tatantus1@gmail.com>
2019-11-14 16:57:40 -0300

    CostPerMinute class green + refactor
    Created CostPerMinuteTests class, made a test method to test CostPerMinute properties and then made CostPerMinute class

---
+++ Obligatorio 1 - DA1/BusinessLogic.Test/CostPerMinuteTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessLogic.Test
+{
+    [TestClass]
+    public class CostPerMinuteTests
+    {
+        [TestMethod]
+        public void CreateCostPerMinute()
+        {
+            CostPerMinute cost = new CostPerMinute
+            {
+                Value = 2,
+                CountryTag = "UY"
+            };
+            Assert.AreEqual(cost.Value, 2);
+            Assert.AreEqual(cost.CountryTag, "UY");
+        }
+    }
+}
---
+++ Obligatorio 1 - DA1/BusinessLogic/CostPerMinute.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLogic
+{
+    public class CostPerMinute
+    {
+        public int ID { get; set; }
+        public int Value { get; set; }
+        public string CountryTag { get; set; }
+        public CostPerMinute() { }
+    }
+}
