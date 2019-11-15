21f8a182bf58a1a930c8981e2e6c7c78953a8092
Sebastián Bañales <tatantus1@gmail.com>
2019-11-15 01:52:53 -0300

    IRepository interface created
    This interface contains the firms for the different operations that are defined by the business rules of the App

---
+++ Obligatorio 1 - DA1/BusinessLogic/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+
+        void Update(T entity);
+
+        IEnumerable<T> GetAll();
+
+        IEnumerable<T> GetByCondition(Expression<Func<T, bool>> expression);
+
+        T Get(string key, string countryTag);
+    }
+}
