25964f01492cd9db05b9b12da23e38a32474d3fe
Sebastián Bañales <tatantus1@gmail.com>
2019-11-15 02:37:53 -0300

    AccountRepository class and test class
    Created a class which will implement the IRepository interface and will implement its operations.

---
+++ Obligatorio 1 - DA1/BusinessLogic.Test/AccountRepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BusinessLogic.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusinessLogic.Test
+{
+    class AccountRepositoryTest
+    {
+        private AccountRepository accountRepository;
+
+        [TestInitialize]
+        public void SetUpPurchaseRepository()
+        {
+            accountRepository = new AccountRepository()
+            {
+                Context = new ParkingContext()
+            };
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            accountRepository.Empty();
+        }
+    }
+}
---
+++ Obligatorio 1 - DA1/BusinessLogic/AccountRepository.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLogic.Exceptions;
+using System;
+using System.Data.Common;
+using System.Linq;
+
+namespace BusinessLogic
+{
+    public class AccountRepository
+    {
+        public ParkingContext Context { get; set; }
+
+        public void Empty()
+        {
+            try
+            {
+                foreach (Purchase p in Context.Purchases.ToList())
+                {
+                    Context.Purchases.Remove(p);
+                    Context.SaveChanges();
+                }
+            }
+            catch (DbException ex)
+            {
+                throw new DatabaseException("Database Error", ex);
+            }
+        }
+    }
+}
