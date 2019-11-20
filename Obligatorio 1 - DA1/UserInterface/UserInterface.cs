628ea72f7fbbaf8e7a25d0780ae2d2f7c9874cd7
Sebastián Bañales <tatantus1@gmail.com>
2019-11-20 16:18:10 -0300

    Bugs fixed
    Fixed a bug on ActivePurchase window when not selecting an hour on the dateTimePicker and leaving the default one

--- Obligatorio 1 - DA1/UserInterface/UserInterface.cs
+++ Obligatorio 1 - DA1/UserInterface/UserInterface.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Drawing;
 using System.Linq;
 using System.Text;
